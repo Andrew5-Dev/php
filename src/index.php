8f44ed5aabeb462952d4abc341897d1e2cca3c39
Andrew5-Dev <topchiy.ap@gmail.com>
2024-07-06 20:35:43 +0300

    hw2

--- src/index.php
+++ src/index.php
@@ -5,10 +5,10 @@ $name = trim(fgets(STDIN));
 
 echo "Hello, $name!\nI am good to meet you!\n";
 
-echo "Ender Number 1\n";
+echo "Enter Number 1\n";
 $number1 = (int)fgets(STDIN);
 
-echo "Ender Number 2\n";
+echo "Enter Number 2\n";
 $number2 = (int)fgets(STDIN);
 
 echo "Sum = " . ($number1 + $number2) . "\n";
