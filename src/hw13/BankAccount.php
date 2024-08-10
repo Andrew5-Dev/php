3666beeed1276b2cee47c1fbe230853d68442edf
Andrew5-Dev <topchiy.ap@gmail.com>
2024-08-10 20:30:16 +0300

    hw13

--- src/hw13/BankAccount.php
+++ src/hw13/BankAccount.php
@@ -1,39 +1,46 @@
 <?php
 
 namespace hw13;
+use Exception;
+
 class BankAccount
 {
-    private $accountNumber;
-    private $balance;
+    private int $accountNumber;
+    private float $balance;
 
-    public function __construct($accountNumber)
+    public function __construct(int $accountNumber, float $balance)
     {
         $this->accountNumber = $accountNumber;
-        $this->balance = 0;
+        $this->balance = $balance;
     }
 
-    public function getAccountNumber()
+    public function getAccountNumber() : int
     {
         return $this->accountNumber;
     }
 
-    public function getBalance()
+    public function getBalance() : float
     {
         return $this->balance;
     }
 
-    public function deposit($amount)
+    public function deposit(float $amount): void
     {
         $this->balance += $amount;
     }
 
-    public function withdraw($amount)
+    /**
+     * @throws Exception
+     */
+    public function withdraw(float $amount) : float
     {
         if ($this->balance >= $amount) {
             $this->balance -= $amount;
         } else {
-            echo "На балансі аккаунту не достатньо коштів!" . PHP_EOL;
+           return  throw new Exception("На балансі аккаунту не достатньо коштів!");
         }
+        return $this->balance;
     }
+
 }
 
--- src/hw13/index.php
+++ src/hw13/index.php
@@ -4,7 +4,7 @@ use hw13\BankAccount;
 
 require_once __DIR__  . '/BankAccount.php';
 
-$account = new BankAccount("12");
+$account = new BankAccount("12", 100);
 
 echo "Інформація по рахунку: " . PHP_EOL;
 echo "Номер рахунку: №" . $account->getAccountNumber() . PHP_EOL;
@@ -18,10 +18,16 @@ echo "Інформація по рахунку: " . PHP_EOL;
 echo "Номер рахунку: №" . $account->getAccountNumber() . PHP_EOL;
 echo "Баланс: " . $account->getBalance() . " Грн". PHP_EOL;
 
+try {
 echo "Скільки зняти коштів" . PHP_EOL;
 $argument = (float)fgets(STDIN);
 $account->withdraw($argument);
+}
+catch (Exception $exception) {
+    echo $exception->getMessage() . PHP_EOL;
+}
 
 echo "Інформація по рахунку: " . PHP_EOL;
 echo "Номер рахунку: №" . $account->getAccountNumber() . PHP_EOL;
 echo "Баланс: " . $account->getBalance() . " Грн". PHP_EOL;
+
